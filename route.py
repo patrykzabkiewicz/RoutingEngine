fb5ecd4198e35305a737b396252a2ad2aa1ae6bf
Patryk Ząbkiewicz <pzabkiewicz@gmail.com>
2015-08-02 20:29:01 +0200

    route class description

--- route.py
+++ route.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+""" Route Class
+Basic class to represent a route from A to B
+
+Author: Patryk Zabkiewicz
+Date:	2015.08.02
+
+"""
 
 class Route:
 	def __init__(self,route={}):
